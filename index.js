f95e2dff0d68b9a89fb70601fa7d0d2d37ba51b3
BonjourYY <fqymtu@gmail.com>
2021-12-16 16:56:55 +0800

    v1.2

--- index.js
+++ index.js
@@ -3,22 +3,34 @@ import './style.css';
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-// ctx.fillStyle = 'green';
-// ctx.fillRect(0, 0, 100, 100);
 
-// 画三角形
-// ctx.fillStyle = 'red';
-// ctx.beginPath();
-// ctx.moveTo(180, 180);
-// ctx.lineTo(200, 180);
-// ctx.lineTo(200, 200);
-// ctx.fill();
+var painting = false;
+var lastPoint = { x: undefined, y: undefined };
 
-// 画圆
-// ctx.fillStyle = 'yellow';
-// ctx.beginPath();
-// ctx.arc(250, 250, 20, 0, Math.PI);
-// ctx.stroke();
+canvas.onmousedown = function (x) {
+  painting = true;
+  var positionX = x.clientX;
+  var positionY = x.clientY;
+  lastPoint = { x: positionX, y: positionY };
+  console.log(lastPoint);
+  drawCircle(positionX, positionY, 1);
+};
+
+canvas.onmousemove = function (x) {
+  if (painting) {
+    var positionX = x.clientX;
+    var positionY = x.clientY;
+    var newPoint = { x: positionX, y: positionY };
+    drawCircle(positionX, positionY, 1);
+    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+    lastPoint = newPoint;
+  } else {
+  }
+};
+
+canvas.onmouseup = function (x) {
+  painting = false;
+};
 
 function drawCircle(x, y, radius) {
   // 画圆
@@ -28,27 +40,12 @@ function drawCircle(x, y, radius) {
   ctx.fill();
 }
 
-// drawCircle(250, 250, 20);
-var painting = false;
-
-canvas.onmousedown = function (x) {
-  painting = true;
-  var positionX = x.clientX;
-  var positionY = x.clientY;
-  console.log(positionX, positionY);
-  drawCircle(positionX, positionY, 20);
-};
-
-canvas.onmousemove = function (x) {
-  if (painting) {
-    var positionX = x.clientX;
-    var positionY = x.clientY;
-    console.log(positionX, positionY);
-    drawCircle(positionX, positionY, 20);
-  } else {
-  }
-};
-
-canvas.onmouseup = function (x) {
-  painting = false;
-};
+function drawLine(x1, y1, x2, y2) {
+  ctx.strokeStyle = 'yellow';
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineWidth = 5;
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.closePath();
+}
