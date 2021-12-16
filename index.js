c27f55aafe08e2667eb113eba9e79af81d3cbfbd
BonjourYY <fqymtu@gmail.com>
2021-12-16 16:25:46 +0800

    v1.1

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 // Import stylesheets
 import './style.css';
 
-// 获取canvas
 var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+// ctx.fillStyle = 'green';
+// ctx.fillRect(0, 0, 100, 100);
 
-// 设置标记，判断用户点击之后开始绘画。
+// 画三角形
+// ctx.fillStyle = 'red';
+// ctx.beginPath();
+// ctx.moveTo(180, 180);
+// ctx.lineTo(200, 180);
+// ctx.lineTo(200, 200);
+// ctx.fill();
+
+// 画圆
+// ctx.fillStyle = 'yellow';
+// ctx.beginPath();
+// ctx.arc(250, 250, 20, 0, Math.PI);
+// ctx.stroke();
+
+function drawCircle(x, y, radius) {
+  // 画圆
+  ctx.fillStyle = 'yellow';
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+// drawCircle(250, 250, 20);
 var painting = false;
 
-// 监听鼠标点击
 canvas.onmousedown = function (x) {
   painting = true;
   var positionX = x.clientX;
   var positionY = x.clientY;
-  var point = document.createElement('div');
-  canvas.appendChild(point);
-  point.style =
-    'width:10px;height:10px;background-color:red;border-radius:50%;position:absolute;left:' +
-    (positionX - 5) +
-    'px;' +
-    'top:' +
-    (positionY - 5) +
-    'px;';
-  // console.log(Number(point.style.width));
+  console.log(positionX, positionY);
+  drawCircle(positionX, positionY, 20);
 };
 
-// 监听鼠标滑动
 canvas.onmousemove = function (x) {
   if (painting) {
     var positionX = x.clientX;
     var positionY = x.clientY;
-    var point = document.createElement('div');
-    canvas.appendChild(point);
-    point.style =
-      'width:10px;height:10px;background-color:red;border-radius:50%;position:absolute;left:' +
-      (positionX - 5) +
-      'px;' +
-      'top:' +
-      (positionY - 5) +
-      'px;';
+    console.log(positionX, positionY);
+    drawCircle(positionX, positionY, 20);
   } else {
   }
 };
 
-// 监听用户松开鼠标;
-canvas.onmouseup = function () {
+canvas.onmouseup = function (x) {
   painting = false;
 };
