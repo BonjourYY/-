e84eb8fc4b1fd31da6ee12f379b0386b58ebb50d
BonjourYY <fqymtu@gmail.com>
2021-12-16 19:18:15 +0800

    v1.3

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 // Import stylesheets
 import './style.css';
 
+// 获取canvas
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var eraserEnabled = false;
 
-var painting = false;
-var lastPoint = { x: undefined, y: undefined };
+// 初始化canvas大小，并在拖动窗口时，自动设置canvas的大小。
+autoSetCanvasSize(canvas);
 
-canvas.onmousedown = function (x) {
-  painting = true;
-  var positionX = x.clientX;
-  var positionY = x.clientY;
-  lastPoint = { x: positionX, y: positionY };
-  console.log(lastPoint);
-  drawCircle(positionX, positionY, 1);
+listenToMouse(canvas);
+
+var actions = document.getElementsByClassName('actions')[0];
+
+eraser.onclick = function () {
+  eraserEnabled = true;
+  actions.className = 'actions x';
 };
 
-canvas.onmousemove = function (x) {
-  if (painting) {
-    var positionX = x.clientX;
-    var positionY = x.clientY;
-    var newPoint = { x: positionX, y: positionY };
-    drawCircle(positionX, positionY, 1);
-    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-    lastPoint = newPoint;
-  } else {
-  }
+brush.onclick = function () {
+  eraserEnabled = false;
+  actions.className = 'actions';
 };
 
-canvas.onmouseup = function (x) {
-  painting = false;
-};
+////////////////////////////////////////
 
-function drawCircle(x, y, radius) {
-  // 画圆
-  ctx.fillStyle = 'yellow';
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2);
-  ctx.fill();
+function autoSetCanvasSize(canvas) {
+  initCanvas(canvas);
+  window.onresize = function () {
+    initCanvas(canvas);
+  };
+}
+
+function initCanvas(canvas) {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
 }
 
 function drawLine(x1, y1, x2, y2) {
@@ -48,4 +45,40 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineTo(x2, y2);
   ctx.stroke();
   ctx.closePath();
+}
+
+function listenToMouse(canvas) {
+  var using = false;
+  var lastPoint = { x: undefined, y: undefined };
+
+  canvas.onmousedown = function (x) {
+    var positionX = x.clientX;
+    var positionY = x.clientY;
+    using = true;
+    if (eraserEnabled) {
+      ctx.clearRect(positionX - 5, positionY - 5, 10, 10);
+    } else {
+      lastPoint = { x: positionX, y: positionY };
+      console.log(lastPoint);
+    }
+  };
+
+  canvas.onmousemove = function (x) {
+    var positionX = x.clientX;
+    var positionY = x.clientY;
+    if (!using) {
+      return;
+    }
+    if (eraserEnabled) {
+      ctx.clearRect(positionX - 5, positionY - 5, 10, 10);
+    } else {
+      var newPoint = { x: positionX, y: positionY };
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+      lastPoint = newPoint;
+    }
+  };
+
+  canvas.onmouseup = function (x) {
+    using = false;
+  };
 }
